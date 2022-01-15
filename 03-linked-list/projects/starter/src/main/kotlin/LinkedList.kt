01e019197339b7ba5c1aae4c6956877114d833de
Jacek Tymicki <j.tymicki@eyeo.com>
2022-01-15 15:42:26 +0100

    Remove last operation

--- 03-linked-list/projects/starter/src/main/kotlin/LinkedList.kt
+++ 03-linked-list/projects/starter/src/main/kotlin/LinkedList.kt
@@ -33,4 +33,63 @@ class LinkedList<T : Any> {
         tail = newNode
         size++
     }
+
+    fun nodeAt(index: Int): Node<T>? {
+        // 1
+        var currentNode = head
+        var currentIndex = 0
+        // 2
+        while (currentNode != null && currentIndex < index) {
+            currentNode = currentNode.next
+            currentIndex++
+        }
+        return currentNode
+    }
+
+    fun insert(value: T, afterNode: Node<T>): Node<T> {
+        // 1
+        if (tail == afterNode) {
+            append(value)
+            return tail!!
+        }
+        // 2
+        val newNode = Node(value = value, next = afterNode.next)
+        // 3
+        afterNode.next = newNode
+        size++
+        return newNode
+    }
+
+    fun pop(): T? {
+        if (isEmpty()) return null
+        val result = head?.value
+        head = head?.next
+        size--
+        if (isEmpty()) {
+            tail = null
+        }
+        return result
+    }
+
+    fun removeLast(): T? {
+        // 1
+        val head = head ?: return null
+        // 2
+        if (head.next == null) return pop()
+        // 3
+        size--
+        // 4
+        var prev = head
+        var current = head
+        var next = current.next
+        while (next != null) {
+            prev = current
+            current = next
+            next = current.next
+        }
+        // 5
+        prev.next = null
+        tail = prev
+        return current.value
+    }
 }
--- 03-linked-list/projects/starter/src/main/kotlin/Main.kt
+++ 03-linked-list/projects/starter/src/main/kotlin/Main.kt
@@ -29,4 +29,39 @@ fun main() {
         list.append(3)
         println(list)
     }
+
+    "inserting at a particular index" example {
+        val list = LinkedList<Int>()
+        list.push(3)
+        list.push(2)
+        list.push(1)
+        println("Before inserting: $list")
+        var middleNode = list.nodeAt(1)!!
+        for (i in 1..3) {
+            middleNode = list.insert(-1 * i, middleNode)
+        }
+        println("After inserting: $list")
+    }
+
+    "pop" example {
+        val list = LinkedList<Int>()
+        list.push(3)
+        list.push(2)
+        list.push(1)
+        println("Before popping list: $list")
+        val poppedValue = list.pop()
+        println("After popping list: $list")
+        println("Popped value: $poppedValue")
+    }
+
+    "removing the last node" example {
+        val list = LinkedList<Int>()
+        list.push(3)
+        list.push(2)
+        list.push(1)
+        println("Before removing last node: $list")
+        val removedValue = list.removeLast()
+        println("After removing last node: $list")
+        println("Removed value: $removedValue")
+    }
 }
