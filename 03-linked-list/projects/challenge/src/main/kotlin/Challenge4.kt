c273905d4a6479d63dfb3dfdf119bd85c6a065df
Jacek Tymicki <j.tymicki@eyeo.com>
2022-01-19 13:18:21 +0100

    minor refactor

--- 03-linked-list/projects/challenge/src/main/kotlin/Challenge4.kt
+++ 03-linked-list/projects/challenge/src/main/kotlin/Challenge4.kt
@@ -35,39 +35,39 @@
  */
 
 fun <T : Comparable<T>> LinkedList<T>.mergeSorted(
-  otherList: LinkedList<T>
+    otherList: LinkedList<T>
 ): LinkedList<T> {
-  if (this.isEmpty()) return otherList
-  if (otherList.isEmpty()) return this
+    if (this.isEmpty()) return otherList
+    if (otherList.isEmpty()) return this
 
-  val result = LinkedList<T>()
+    val result = LinkedList<T>()
 
-  var left = nodeAt(0)
-  var right = otherList.nodeAt(0)
+    var left = nodeAt(0)
+    var right = otherList.nodeAt(0)
 
-  while (left != null && right != null) {
-    if (left.value < right.value) {
-      left = append(result, left)
-    } else {
-      right = append(result, right)
+    while (left != null && right != null) {
+        if (left.value < right.value) {
+            left = append(result, left)
+        } else {
+            right = append(result, right)
+        }
     }
-  }
 
-  while (left != null) {
-    left = append(result, left)
-  }
+    while (left != null) {
+        left = append(result, left)
+    }
 
-  while (right != null) {
-    right = append(result, right)
-  }
+    while (right != null) {
+        right = append(result, right)
+    }
 
-  return result
+    return result
 }
 
 private fun <T : Comparable<T>> append(
-  result: LinkedList<T>,
-  node: Node<T>
+    result: LinkedList<T>,
+    node: Node<T>
 ): Node<T>? {
-  result.append(node.value)
-  return node.next
+    result.append(node.value)
+    return node.next
 }
